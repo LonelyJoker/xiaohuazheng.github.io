1240930c0ef95bb7c9a9b239c38cd08f6b830394
xiaohuazheng <463027542@qq.com>
2017-09-24 18:05:18 +0800

    update && optimize

--- js/gnmenu.js
+++ js/gnmenu.js
@@ -1,22 +1,17 @@
 var menuBtn = document.getElementById('gn-menu-btn'),
     menuSlide = document.getElementById('gn-menu-slide');
-
 menuBtn.addEventListener('mouseenter', function() {
     menuSlide.classList.add('gn-open-all');
 });
-
 menuBtn.addEventListener('click', function() {
     menuSlide.classList.add('gn-open-all');
 });
-
 menuSlide.addEventListener('mouseenter', function() {
     menuSlide.classList.add('gn-open-all');
 });
-
 menuBtn.addEventListener('mouseleave', function() {
     menuSlide.classList.remove('gn-open-all');
 });
-
 menuSlide.addEventListener('mouseleave', function() {
     menuSlide.classList.remove('gn-open-all');
 });
