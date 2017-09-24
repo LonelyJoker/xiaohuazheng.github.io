ed9a1725d252a80b6ede14b03ac3ecb5b38fab73
xiaohuazheng <463027542@qq.com>
2017-09-24 18:30:52 +0800

    update && optimize

--- js/gnmenu.js
+++ js/gnmenu.js
@@ -1,17 +1,17 @@
-var menuBtn = document.getElementById('gn-menu-btn'),
-    menuSlide = document.getElementById('gn-menu-slide');
+var menuBtn = document.getElementById('nav-menu-btn'),
+    menuSlide = document.getElementById('nav-menu-slide');
 menuBtn.addEventListener('mouseenter', function() {
-    menuSlide.classList.add('gn-open-all');
+    menuSlide.classList.add('nav-open-all');
 });
 menuBtn.addEventListener('click', function() {
-    menuSlide.classList.add('gn-open-all');
+    menuSlide.classList.add('nav-open-all');
 });
 menuSlide.addEventListener('mouseenter', function() {
-    menuSlide.classList.add('gn-open-all');
+    menuSlide.classList.add('nav-open-all');
 });
 menuBtn.addEventListener('mouseleave', function() {
-    menuSlide.classList.remove('gn-open-all');
+    menuSlide.classList.remove('nav-open-all');
 });
 menuSlide.addEventListener('mouseleave', function() {
-    menuSlide.classList.remove('gn-open-all');
+    menuSlide.classList.remove('nav-open-all');
 });
